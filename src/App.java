e07ead73bde682de0d0efc04d555b17ae0108d22
afsalJ <mohamedafsal.jvf@gmail.com>
2024-12-08 16:28:20 +0530

    class/BillService

--- src/App.java
+++ src/App.java
@@ -1,4 +1,14 @@
+import java.util.List;
+
+import entity.Discount;
+import entity.Item;
+import service.BillService;
+
 public class App {
     public static void main(String[] args) throws Exception {
+        BillService billService = new BillService();
+        System.out.println(
+        billService.getBill(List.of(new Item("Headphones (1 @ 3000 Rs)"),new Item("Mobile case (1 @ 500 Rs)")), new Discount("15% on orders above 3000 Rs"))
+        );
     }
 }
---
+++ src/service/BillService.java
@@ -0,0 +1,51 @@
+package service;
+
+import java.util.List;
+
+import entity.Discount;
+import entity.Item;
+
+public class BillService {
+    private Double totalPrice;
+    private String calculationTotalPrice;
+
+    private void calculateTotalPrice(List<Item> items){
+        totalPrice = 0d;
+        calculationTotalPrice = "";
+        for(Item item : items){
+            if(!calculationTotalPrice.isBlank())calculationTotalPrice += " + ";
+            Double currentItemPrice = item.getTotalPrice();
+            totalPrice = totalPrice + currentItemPrice;
+            calculationTotalPrice += currentItemPrice;
+        }
+        calculationTotalPrice += (" = "+totalPrice);
+    }
+    
+    public String getBill(List<Item> items, Discount discount){
+        calculateTotalPrice(items);
+        Double discountValue = discount.getDiscount(totalPrice);
+        
+        return
+            (
+                "Amount to be paid: "
+                + (totalPrice-discountValue)
+                + " Rs "
+            )
+            +
+            "[Calcualation:"
+            +calculationTotalPrice
+            +(
+                discountValue!=0d ?
+                " - "
+                +discountValue
+                +" ("
+                +discount.getStatement()
+                +") ="
+                +(totalPrice-discountValue)
+                : ""
+            )
+            +" Rs]" 
+        ;
+        
+    }
+}
