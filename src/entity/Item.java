412d422095fca8dda916b0965382e56c7a9f1270
afsalJ <mohamedafsal.jvf@gmail.com>
2024-12-08 14:42:51 +0530

    Item class added

---
+++ src/entity/Item.java
@@ -0,0 +1,14 @@
+package entity;
+
+public class Item {
+    private String name;
+    private Double price;
+
+    public String getName(){
+        return name;
+    }
+
+    public Double getPrice(){
+        return price;
+    }
+}
